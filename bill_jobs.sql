91fc1de2ce4f530f8a35ba1d8ff6c1018978c4ec
BayronZ <bayron.zavalag@gmail.com>
2022-04-19 20:58:52 -0400

    Se creó y cargó la base de datos

---
+++ bill_jobs.sql
@@ -0,0 +1,20 @@
+-- 1. Cargar el respaldo de la base de datos unidad2.sql. (2 Puntos)
+--Se elimino el autocmmit
+-- bill_jobs=# \set AUTOCOMMIT OFF
+-- bill_jobs=# \echo :AUTOCOMMIT
+-- OFF
+
+--Se creó la base de datos
+--mrcoco=# CREATE DATABASE bill_jobs;
+-- CREATE DATABASE
+
+-- Se cargó el archivo unidad2.sql
+--psql -U mrcoco bill_jobs < unidad2.sql
+-- SET
+-- SET
+-- SET
+-- SET
+-- SET
+--  set_config 
+-- ------------
+-- (1 row)
